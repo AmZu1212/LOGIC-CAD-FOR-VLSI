b052a9285eed564e7c65f999c3ac66652832b02e
Amir Zuabi <zuabiamir123@gmail.com>
2025-12-09 17:01:27 +0200

    formatting and comment changes

--- HCM/wet01/HW1ex2.cc
+++ HCM/wet01/HW1ex2.cc
@@ -13,212 +13,227 @@ using namespace std;
 
 bool verbose = false;
 
+int main(int argc, char **argv)
+{
+	int argIdx = 1;
+	int anyError = 0;
+	unsigned int i;
+	vector<string> vlgFiles;
 
-int main(int argc, char **argv) {
-  int argIdx = 1;
-  int anyError = 0;
-  unsigned int i;
-  vector<string> vlgFiles;
-  
-  if (argc < 3) {
-    anyError++;
-  } else {
-    if (!strcmp(argv[argIdx], "-v")) {
-      argIdx++;
-      verbose = true;
-    }
-    for (;argIdx < argc; argIdx++) {
-      vlgFiles.push_back(argv[argIdx]);
-    }
-    
-    if (vlgFiles.size() < 2) {
-      cerr << "-E- At least top-level and single verilog file required for spec model" << endl;
-      anyError++;
-    }
-  }
+	if (argc < 3)
+	{
+		anyError++;
+	}
+	else
+	{
+		if (!strcmp(argv[argIdx], "-v"))
+		{
+			argIdx++;
+			verbose = true;
+		}
+		for (; argIdx < argc; argIdx++)
+		{
+			vlgFiles.push_back(argv[argIdx]);
+		}
 
-  if (anyError) {
-    cerr << "Usage: " << argv[0] << "  [-v] top-cell file1.v [file2.v] ... \n";
-    exit(1);
-  }
- 
-  set< string> globalNodes;
-  globalNodes.insert("VDD");
-  globalNodes.insert("VSS");
-  
-  hcmDesign* design = new hcmDesign("design");
-  string cellName = vlgFiles[0];
-  for (i = 1; i < vlgFiles.size(); i++) {
-    printf("-I- Parsing verilog %s ...\n", vlgFiles[i].c_str());
-    if (!design->parseStructuralVerilog(vlgFiles[i].c_str())) {
-      cerr << "-E- Could not parse: " << vlgFiles[i] << " aborting." << endl;
-      exit(1);
-    }
-  }
-  
+		if (vlgFiles.size() < 2)
+		{
+			cerr << "-E- At least top-level and single verilog file required for spec model" << endl;
+			anyError++;
+		}
+	}
 
-  /*direct output to file*/
-  string fileName = cellName + string(".rank");
-  ofstream fv(fileName.c_str());
-  if (!fv.good()) {
-    cerr << "-E- Could not open file:" << fileName << endl;
-    exit(1);
-  }
+	if (anyError)
+	{
+		cerr << "Usage: " << argv[0] << "  [-v] top-cell file1.v [file2.v] ... \n";
+		exit(1);
+	}
 
-  hcmCell *topCell = design->getCell(cellName);
-  if (!topCell) {
-    printf("-E- could not find cell %s\n", cellName.c_str());
-    exit(1);
-  }
-  
-  fv << "file name: " << fileName << endl;
-  
+	set<string> globalNodes;
+	globalNodes.insert("VDD");
+	globalNodes.insert("VSS");
 
-  /* assign your answer for HW1ex2 to maxRankVector 
-   * maxRankVector is a vector of pairs of type <int, string>,
-   * int is the rank of the instance,
-   * string is the name of the instance
-   * Note - maxRankVector should be sorted.
-  */
-  vector<pair<int, string>> maxRankVector;
-  //---------------------------------------------------------------------------------//
-  //enter your code here 
-  //---------------------------------------------------------------------------------//
+	hcmDesign *design = new hcmDesign("design");
+	string cellName = vlgFiles[0];
+	for (i = 1; i < vlgFiles.size(); i++)
+	{
+		printf("-I- Parsing verilog %s ...\n", vlgFiles[i].c_str());
+		if (!design->parseStructuralVerilog(vlgFiles[i].c_str()))
+		{
+			cerr << "-E- Could not parse: " << vlgFiles[i] << " aborting." << endl;
+			exit(1);
+		}
+	}
 
-  // flatten the design to operate on a flat netlist
-  hcmCell *flatCell = hcmFlatten(cellName + string("_flat_rank"), topCell, globalNodes);
+	/*direct output to file*/
+	string fileName = cellName + string(".rank");
+	ofstream fv(fileName.c_str());
+	if (!fv.good())
+	{
+		cerr << "-E- Could not open file:" << fileName << endl;
+		exit(1);
+	}
 
-  // gather instances
-  map<string, hcmInstance *> allInsts = flatCell->getInstances();
+	hcmCell *topCell = design->getCell(cellName);
+	if (!topCell)
+	{
+		printf("-E- could not find cell %s\n", cellName.c_str());
+		exit(1);
+	}
 
-  // ranking via topological propagation
-  const string TOP_SRC = "__TOP_INPUT__";
-  const int NEG_INF = -1000000;
-  map<string, int> rank;
-  map<string, int> indeg;
-  map<string, vector<string>> adj;
+	fv << "file name: " << fileName << endl;
 
-  // init rank/indeg for all instances
-  for (auto &ipair : allInsts)
-  {
-    rank[ipair.first] = NEG_INF;
-    indeg[ipair.first] = 0;
-  }
-  rank[TOP_SRC] = -1;
-  indeg[TOP_SRC] = 0;
+	/* assign your answer for HW1ex2 to maxRankVector
+	 * maxRankVector is a vector of pairs of type <int, string>,
+	 * int is the rank of the instance,
+	 * string is the name of the instance
+	 * Note - maxRankVector should be sorted.
+	 */
+	vector<pair<int, string>> maxRankVector;
 
-  // build edges driver->sink
-  set<pair<string, string>> edges;
-  for (auto &npair : flatCell->getNodes())
-  {
-    hcmNode *node = npair.second;
-    if (globalNodes.find(node->getName()) != globalNodes.end())
-    {
-      continue;
-    }
+	// Written by:
+	//				Amir Zuabi 		 - 212606222
+	//				Alexey Vasilayev - 323686683
+	//  === HW1ex2 ANSWER STARTS HERE ===
 
-    vector<string> drivers;
-    vector<string> sinks;
+	// flatten the design to operate on a flat netlist
+	hcmCell *flatCell = hcmFlatten(cellName + string("_flat_rank"), topCell, globalNodes);
 
-    for (auto &ippair : node->getInstPorts())
-    {
-      hcmInstPort *ip = ippair.second;
-      hcmPort *mport = ip->getPort();
-      if (!mport)
-      {
-        continue;
-      }
-      string iname = ip->getInst()->getName();
-      if (mport->getDirection() == OUT || mport->getDirection() == IN_OUT)
-      {
-        drivers.push_back(iname);
-      }
-      if (mport->getDirection() == IN || mport->getDirection() == IN_OUT)
-      {
-        sinks.push_back(iname);
-      }
-    }
+	// gather instances
+	map<string, hcmInstance *> allInsts = flatCell->getInstances();
 
-    hcmPort *p = node->getPort();
-    if (p && (p->getDirection() == IN || p->getDirection() == IN_OUT))
-    {
-      drivers.push_back(TOP_SRC);
-    }
+	// ranking via topological propagation
+	const string TOP_SRC = "__TOP_INPUT__";
+	const int NEG_INF = -1000000;
+	map<string, int> rank;
+	map<string, int> indeg;
+	map<string, vector<string>> adj;
 
-    for (const auto &d : drivers)
-    {
-      for (const auto &s : sinks)
-      {
-        if (d == s)
-        {
-          continue;
-        }
-        pair<string, string> e(d, s);
-        if (edges.insert(e).second)
-        {
-          adj[d].push_back(s);
-          indeg[s]++; // ensure entry exists
-          if (indeg.find(d) == indeg.end())
-          {
-            indeg[d] = 0;
-          }
-        }
-      }
-    }
-  }
+	// initialize all "vertices" ranks to -inf
+	for (auto &ipair : allInsts)
+	{
+		rank[ipair.first] = NEG_INF;
+		indeg[ipair.first] = 0;
+	}
+	rank[TOP_SRC] = -1;
+	indeg[TOP_SRC] = 0;
 
-  // Kahn topological traversal, longest-path style
-  queue<string> q;
-  for (auto &ipair : indeg)
-  {
-    if (ipair.second == 0)
-    {
-      if (rank.find(ipair.first) == rank.end())
-      {
-        rank[ipair.first] = NEG_INF;
-      }
-      q.push(ipair.first);
-    }
-  }
+	// build edges driver->sink
+	set<pair<string, string>> edges;
+	for (auto &npair : flatCell->getNodes())
+	{
+		hcmNode *node = npair.second;
+		if (globalNodes.find(node->getName()) != globalNodes.end())
+		{
+			continue;
+		}
 
-  while (!q.empty())
-  {
-    string cur = q.front();
-    q.pop();
-    int curRank = rank[cur];
-    for (const auto &nxt : adj[cur])
-    {
-      if (curRank != NEG_INF && rank[nxt] < curRank + 1)
-      {
-        rank[nxt] = curRank + 1;
-      }
-      indeg[nxt]--;
-      if (indeg[nxt] == 0)
-      {
-        q.push(nxt);
-      }
-    }
-  }
+		vector<string> drivers;
+		vector<string> sinks;
 
-  // build and sort output
-  for (auto &ipair : rank)
-  {
-    if (ipair.second >= 0 && ipair.first != TOP_SRC)
-    {
-      maxRankVector.push_back(make_pair(ipair.second, ipair.first));
-    }
-  }
-  sort(maxRankVector.begin(), maxRankVector.end(),
-       [](const pair<int, string> &a, const pair<int, string> &b)
-       {
-         if (a.first != b.first)
-           return a.first < b.first;
-         return a.second < b.second;
-       });
+		for (auto &ippair : node->getInstPorts())
+		{
+			hcmInstPort *ip = ippair.second;
+			hcmPort *mport = ip->getPort();
+			if (!mport)
+			{
+				continue;
+			}
+			string iname = ip->getInst()->getName();
+			if (mport->getDirection() == OUT || mport->getDirection() == IN_OUT)
+			{
+				drivers.push_back(iname);
+			}
+			if (mport->getDirection() == IN || mport->getDirection() == IN_OUT)
+			{
+				sinks.push_back(iname);
+			}
+		}
 
-  for(auto itr = maxRankVector.begin(); itr != maxRankVector.end(); itr++){
+		hcmPort *p = node->getPort();
+		if (p && (p->getDirection() == IN || p->getDirection() == IN_OUT))
+		{
+			drivers.push_back(TOP_SRC);
+		}
+
+		for (const auto &d : drivers)
+		{
+			for (const auto &s : sinks)
+			{
+				if (d == s)
+				{
+					continue;
+				}
+				pair<string, string> e(d, s);
+				if (edges.insert(e).second)
+				{
+					adj[d].push_back(s);
+					indeg[s]++; // ensure entry exists
+					if (indeg.find(d) == indeg.end())
+					{
+						indeg[d] = 0;
+					}
+				}
+			}
+		}
+	}
+
+	// Kahn topological traversal, longest-path style
+	queue<string> q;
+	for (auto &ipair : indeg)
+	{
+		if (ipair.second == 0)
+		{
+			if (rank.find(ipair.first) == rank.end())
+			{
+				rank[ipair.first] = NEG_INF;
+			}
+			q.push(ipair.first);
+		}
+	}
+
+	while (!q.empty())
+	{
+		string cur = q.front();
+		q.pop();
+		int curRank = rank[cur];
+		for (const auto &nxt : adj[cur])
+		{
+			if (curRank != NEG_INF && rank[nxt] < curRank + 1)
+			{
+				rank[nxt] = curRank + 1;
+			}
+			indeg[nxt]--;
+			if (indeg[nxt] == 0)
+			{
+				q.push(nxt);
+			}
+		}
+	}
+
+	// insert pairs into maxRankVector
+	for (auto &ipair : rank)
+	{
+		if (ipair.second >= 0 && ipair.first != TOP_SRC)
+		{
+			maxRankVector.push_back(make_pair(ipair.second, ipair.first));
+		}
+	}
+
+	// sort by rank, then by name
+	sort(maxRankVector.begin(), maxRankVector.end(),
+		 [](const pair<int, string> &a, const pair<int, string> &b)
+		 {
+			 if (a.first != b.first)
+				 return a.first < b.first;
+			 return a.second < b.second;
+		 });
+
+	// print to file
+	for (auto itr = maxRankVector.begin(); itr != maxRankVector.end(); itr++)
+	{
 		fv << itr->first << " " << itr->second << endl;
 	}
 
-  return(0);
+	return (0);
 }
